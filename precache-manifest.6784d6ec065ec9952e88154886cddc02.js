9a9a1698c0303ca056b4bcea962229f6f6ab2564
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-02-10 22:37:46 +0200

    Updates

---
+++ precache-manifest.6784d6ec065ec9952e88154886cddc02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92630ba7821a07dcb85a56a5d8db7073",
+    "url": "/reviews-project/index.html"
+  },
+  {
+    "revision": "5b429aa876b153cb5c37",
+    "url": "/reviews-project/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/reviews-project/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reviews-project/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b429aa876b153cb5c37",
+    "url": "/reviews-project/static/js/main.b1e5d1d5.chunk.js"
+  },
+  {
+    "revision": "4f8aab8c185f5d608e32",
+    "url": "/reviews-project/static/js/runtime-main.f7486be3.js"
+  }
+]);
